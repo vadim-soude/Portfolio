e1f7729a7aa908f8be07248594d46cdd55114bb9
vadimsoude <vadimsoude@gmail.com>
2024-01-28 21:54:10 +0100

    Fix header / theme switcher :)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component } from "@angular/core";
+import {Component, SecurityContext} from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterLink } from "@angular/router";
+
 
 @Component({
     selector: "app-home",
