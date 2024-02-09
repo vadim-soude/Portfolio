0be8d1be8bbbf29bea3eda9020725eac36994bb3
vadimsoude <vadimsoude@gmail.com>
2024-02-09 10:35:21 +0100

    Add content to the contact page and rework the display of the projet page

--- src/routes/projets/+page.svelte
+++ src/routes/projets/+page.svelte
@@ -1,177 +1,259 @@
-<script>
-</script>
-
 <section>
 	<h1 style="white-space: nowrap">Expériences professionnelles</h1>
+
 	<br><br>
-	<h2>PoudlardRP</h2>
-	<a href="https://www.linkedin.com/company/poudlardrp/">Linkedin du projet.</a><br><br>
-	<div class="info-box">
+
+	<div class="box">
+		<h2>PoudlardRP</h2>
+		<a href="https://www.linkedin.com/company/poudlardrp/">Linkedin du projet.</a><br>
+		<div class="info-box">
 		<span class="text">
-			<u style="font-weight: bold">Informations</u> : Création d'un serveur Minecraft MMORPG sur le thème d'Harry Potter. <br><br>
-			J'ai eu l'occasion de prendre part à ce projet et de son équipe de plus de 100 bénévoles. <br>
-			Cela m'a permis de développer sur un jeu passionnant avec des développeurs brillants où j'ai eu la chance d'apprendre bien plus que je n'aurais pu espérer. <br>
-			Ce projet a été une expérience très enrichissante sur de nombreux domaines. <br>
-			Merci à Lucien, président de Wizards Universe, de m'avoir donné sa confiance dans ce projet.
-		</span><br>
-		<span class="text">
-			<u style="font-weight: bold">Nature</u> : Activité associative.
-		</span><br>
-		<span class="text">
-			<u style="font-weight: bold">Date</u> : Août 2021 - Juin 2023 (Mise en pause du projet).
-		</span><br>
-		<span class="text">
-			<u style="font-weight: bold">Activité</u> : Développeur junior java pendant 6 mois,
-		puis développeur senior java pendant 8 mois pour enfin être directeur du pôle développement.
-	 	</span><br>
-		<span class="text">
-			<u style="font-weight: bold">Repo</u> : Privé - Sous NDA.
-	 	</span><br>
-		<span class="text">
-			<u style="font-weight: bold">Compétences </u> :
+			<span style="font-weight: bold">Activité</span> : Développeur junior java pendant 6 mois,
+			puis développeur senior java pendant 8 mois pour enfin être directeur du pôle développement.
 	 	</span>
-		<div class="list">
-			<ul>
-				<li>- <span style="font-weight: bold" >Programmation orientée objet :</span> Java</li>
-				<li>- <span style="font-weight: bold" >Gestion de dépendances :</span> Maven</li>
-				<li>- <span style="font-weight: bold" >Version Control :</span> Gitlab, Gitea</li>
-				<li>- <span style="font-weight: bold" >Base de données :</span> Mysql, Redis</li>
-				<li>- <span style="font-weight: bold" >Intégration continue :</span> Jenkins</li>
-				<li>- <span style="font-weight: bold" >Mise en production :</span> Docker</li>
-				<li>- <span style="font-weight: bold" >Développement graphique :</span> OpenGL, GLSL</li>
-				<li>- <span style="font-weight: bold" >Résolution de bug :</span> Mantis</li>
-				<li>- <span style="font-weight: bold" >Gestion de projet :</span> Organisation de réunion, Attribution de tâches</li>
-			</ul>
+			<span class="text">
+			<span style="font-weight: bold">Date</span> : Août 2021 - Juin 2023 (Mise en pause du projet).
+		</span>
+			<div class="dropdown">
+				<input class="dropdown-input" id="toggleDropdown1" type="checkbox">
+				<label class="dropdown-label" for="toggleDropdown1"><span class="text"><span
+					style="font-weight: bold">Informations</span> : Création d'un serveur Minecraft MMORPG sur le thème d'Harry Potter.</span></label>
+				<div class="dropdown-content">
+				<span class="text">
+					J'ai eu l'occasion de prendre part à ce projet et de son équipe de plus de 100 bénévoles. <br>
+					Cela m'a permis de développer sur un jeu passionnant avec des développeurs brillants où j'ai eu la chance d'apprendre bien plus que je n'aurais pu espérer. <br>
+					Ce projet a été une expérience très enrichissante sur de nombreux domaines. <br>
+					Merci à Lucien, président de Wizards Universe, de m'avoir donné sa confiance dans ce projet.
+				</span>
+				</div>
+			</div>
+			<div class="dropdown">
+				<input class="dropdown-input" id="toggleDropdown2" type="checkbox">
+				<label class="dropdown-label" for="toggleDropdown2"><span class="text"><span
+					style="font-weight: bold">Compétences acquises ou appliquées</span> :</span></label>
+				<div class="dropdown-content">
+					- <span style="font-weight: bold">Programmation orientée objet :</span> Java<br>
+					- <span style="font-weight: bold">Gestion de dépendances :</span> Maven<br>
+					- <span style="font-weight: bold">Version Control :</span> Gitlab, Gitea<br>
+					- <span style="font-weight: bold">Base de données :</span> Mysql, Redis<br>
+					- <span style="font-weight: bold">Intégration continue :</span> Jenkins<br>
+					- <span style="font-weight: bold">Mise en production :</span> Docker<br>
+					- <span style="font-weight: bold">Développement graphique :</span> OpenGL, GLSL<br>
+					- <span style="font-weight: bold">Résolution de bug :</span> Mantis<br>
+					- <span style="font-weight: bold">Gestion de projet :</span> Organisation de réunion, Attribution de
+					tâches<br>
+				</div>
+			</div>
+			<span class="text">
+			<span style="font-weight: bold">Nature</span> : Activité associative.
+		</span>
+			<span class="text">
+			<span style="font-weight: bold">Repo</span> : Privé - Sous NDA.
+	 	</span>
 		</div>
 	</div>
+
+	<br><br>
+
+	<div class="box">
 	<h2>Mercure Studio</h2>
-	<a href="https://www.linkedin.com/company/mercure-studio-europe/">Linkedin de la société.</a><br><br>
+	<a href="https://www.linkedin.com/company/mercure-studio-europe/">Linkedin de la société.</a><br>
 	<div class="info-box">
 		<span class="text">
-			<u style="font-weight: bold">Informations</u> : Réalisation d'une application "template" dans le cadre du développement de l'offre de SAS. <br><br>
-			Réalisation d'une application ayant pour but de fournir à des restaurateur, leur propre application mobile pour la réservation de tables et de repas à emporter.
-		</span><br>
-		<span class="text">
-			<u style="font-weight: bold">Nature</u> : Stage.
-		</span><br>
-		<span class="text">
-			<u style="font-weight: bold">Date</u> : Avril 2022 - Juin 2022.
-		</span><br>
-		<span class="text">
-			<u style="font-weight: bold">Activité</u> : Développeur Java - Android.
-	 	</span><br>
-		<span class="text">
-			<u style="font-weight: bold">Repo</u> : Privé - Sous NDA.
-	 	</span><br>
-		<span class="text">
-			<u style="font-weight: bold">Compétences </u> :
+			<span style="font-weight: bold">Activité</span> : Développeur Java - Android.
 	 	</span>
-		<div class="list">
-			<ul>
-				<li>- <span style="font-weight: bold" >Programmation orientée objet :</span> Java</li>
-				<li>- <span style="font-weight: bold" >Gestion de dépendances :</span> Maven</li>
-				<li>- <span style="font-weight: bold" >Base de données :</span> Mysql</li>
-				<li>- <span style="font-weight: bold" >Autonomie :</span> Télétravail</li>
-			</ul>
+		<span class="text">
+			<span style="font-weight: bold">Date</span> : Avril 2022 - Juin 2022.
+		</span>
+		<div class="dropdown">
+			<input class="dropdown-input" id="toggleDropdown3" type="checkbox">
+			<label class="dropdown-label" for="toggleDropdown3"><span class="text"><span
+				style="font-weight: bold">Informations</span> : Réalisation d'une application "template" dans le cadre du développement de l'offre de SAS.</span></label>
+			<div class="dropdown-content">
+				<span class="text">
+					Réalisation d'une application ayant pour but de fournir à des restaurateur, leur propre application mobile pour la réservation de tables et de repas à emporter. <br>
+				</span>
+			</div>
 		</div>
+		<div class="dropdown">
+			<input class="dropdown-input" id="toggleDropdown4" type="checkbox">
+			<label class="dropdown-label" for="toggleDropdown4"><span class="text"><span
+				style="font-weight: bold">Compétences acquises ou appliquées</span> :</span></label>
+			<div class="dropdown-content">
+				- <span style="font-weight: bold">Programmation orientée objet :</span> Java<br>
+				- <span style="font-weight: bold">Gestion de dépendances :</span> Maven<br>
+				- <span style="font-weight: bold">Base de données :</span> Mysql<br>
+				- <span style="font-weight: bold">Autonomie :</span> Télétravail<br>
+			</div>
+		</div>
+		<span class="text">
+			<span style="font-weight: bold">Nature</span> : Stage.
+		</span>
+		<span class="text">
+			<span style="font-weight: bold">Repo</span> : Privé - Sous NDA.
+	 	</span>
 	</div>
+	</div>
+
 	<hr>
+
 	<h1 style="white-space: nowrap">Projets personnels</h1><br><br>
+
+	<div class="box">
 	<h2>SAO Reforged</h2>
-	<a href="http://www.saoreforged.fr">Website WIP.</a><br><br>
+	<a href="http://www.saoreforged.fr">Website WIP.</a><br>
 	<div class="info-box">
 		<span class="text">
-			<u style="font-weight: bold">Informations</u> : Création d'un serveur Minecraft MMORPG sur le thème de l'animé Sword Art Online. <br><br>
+			<span style="font-weight: bold">Activité</span> : Fondateur, responsable, manager et développeur du projet.
+	 	</span>
+		<span class="text">
+			<span style="font-weight: bold">Date</span> : Septembre 2022 - En cours.
+		</span>
+		<div class="dropdown">
+			<input class="dropdown-input" id="toggleDropdown5" type="checkbox">
+			<label class="dropdown-label" for="toggleDropdown5"><span class="text"><span
+				style="font-weight: bold">Informations</span> : Création d'un serveur Minecraft MMORPG sur le thème de l'animé Sword Art Online.</span></label>
+			<div class="dropdown-content">
+				<span class="text">
 			Ce projet est soutenu par une petite équipe de bénévoles travaillant entre amis essayant de réaliser un serveur qui peut nous rendre fier. <br>
-			Ce projet me permet de développer de nombreuses compétences et de m'améliorer dans un cadre presque professionnel. <br>
+			Celui-ci me permet de développer de nombreuses compétences et de m'améliorer dans un cadre presque professionnel. <br>
 			J'ai pour ambition de mener à bien ce projet, sortir un serveur qui plaisent aux joueurs et continuer autant que possible.
-		</span><br>
-		<span class="text">
-			<u style="font-weight: bold">Nature</u> : Activité associative (démarches en cours).
-		</span><br>
-		<span class="text">
-			<u style="font-weight: bold">Date</u> : Septembre 2022 - En cours.
-		</span><br>
-		<span class="text">
-			<u style="font-weight: bold">Activité</u> : Fondateur, responsable, manager et développeur du projet.
-	 	</span><br>
-		<span class="text">
-			<u style="font-weight: bold">Repo</u> : Privé.
-	 	</span><br>
-		<span class="text">
-			<u style="font-weight: bold">Compétences </u> :
-	 	</span>
-		<div class="list">
-			<ul>
-				<li>- <span style="font-weight: bold" >Programmation orientée objet :</span> Java</li>
-				<li>- <span style="font-weight: bold" >Gestion de dépendances :</span> Maven</li>
-				<li>- <span style="font-weight: bold" >Version Control :</span> Gitea</li>
-				<li>- <span style="font-weight: bold" >Déploiement de service :</span> Gitea, Gitea-runner, Jira, Jenkins, Mysql, Serveur Minecraft, Traefik, Wireguard, Docker, Angular / Nginx, DotNet</li>
-				<li>- <span style="font-weight: bold" >Administration réseau :</span> Traefik, Wireguad, UNIX</li>
-				<li>- <span style="font-weight: bold" >Base de données :</span> Mysql, Redis</li>
-				<li>- <span style="font-weight: bold" >Intégration continue :</span> Jenkins, Gitea Actions</li>
-				<li>- <span style="font-weight: bold" >Mise en production :</span> Docker, DockerFile custom et docker compose</li>
-				<li>- <span style="font-weight: bold" >Développement graphique :</span> OpenGL, GLSL</li>
-				<li>- <span style="font-weight: bold" >Système :</span> UNIX / Ubuntu</li>
-				<li>- <span style="font-weight: bold" >Résolution de bug :</span> Issues Gitea</li>
-				<li>- <span style="font-weight: bold" >Gestion de projet :</span> Organisation de réunions, Attribution de tâches, Déclaration administrative, Formation, Réalisation de cahier des charges</li>
-			</ul>
+				</span>
+			</div>
 		</div>
+		<div class="dropdown">
+			<input class="dropdown-input" id="toggleDropdown6" type="checkbox">
+			<label class="dropdown-label" for="toggleDropdown6"><span class="text"><span
+				style="font-weight: bold">Compétences acquises ou appliquées</span> :</span></label>
+			<div class="dropdown-content">
+				- <span style="font-weight: bold">Programmation orientée objet :</span> Java<br>
+				- <span style="font-weight: bold">Gestion de dépendances :</span> Maven<br>
+				- <span style="font-weight: bold">Version Control :</span> Gitea<br>
+				- <span style="font-weight: bold">Déploiement de service :</span> Gitea, Gitea-runner, Jira, Jenkins, Mysql, Serveur Minecraft, Traefik, Wireguard, Docker, Angular / Nginx, DotNet<br>
+				- <span style="font-weight: bold">Administration réseau :</span> Traefik, Wireguad, UNIX<br>
+				- <span style="font-weight: bold">Base de données :</span> Mysql, Redis<br>
+				- <span style="font-weight: bold">Intégration continue :</span> Jenkins, Gitea Actions<br>
+				- <span style="font-weight: bold">Mise en production :</span> Docker, DockerFile custom et docker compose<br>
+				- <span style="font-weight: bold">Développement graphique :</span> OpenGL, GLSL<br>
+				- <span style="font-weight: bold">Système :</span> UNIX / Ubuntu<br>
+				- <span style="font-weight: bold">Résolution de bug :</span> Issues Gitea<br>
+				- <span style="font-weight: bold">Gestion de projet :</span> Organisation de réunions, Attribution de tâches, Déclaration administrative, Formation, Réalisation de cahier des charges<br>
+			</div>
+		</div>
+		<span class="text">
+			<span style="font-weight: bold">Nature</span> : Activité associative (démarches en cours).
+		</span>
+		<span class="text">
+			<span style="font-weight: bold">Repo</span> : Privé.
+	 	</span>
+
 	</div>
+	</div>
+
+	<br><br>
+	<div class="box">
 	<h2>Site du BDE InfoContact</h2>
-	<a href="https://www.linkedin.com/company/info-contact/">Linkedin du bureau.</a><br><br>
+	<a href="https://www.linkedin.com/company/info-contact/">Linkedin du bureau.</a><br>
 	<div class="info-box">
 		<span class="text">
-			<u style="font-weight: bold">Informations</u> : Projet de réalisation d'un site d'information et de vente du BDE de mon IUT. <br><br>
-			Ce projet a été réalisé en équipe dans le cadre de la SAE du second semestre de ma troisième année de BUT Informatique. <br>
-			Ce projet m'a permis d'apprendre plusieurs nouveaux frameworks et environnement de travail. <br>
-			Je me suis investi au maximum dans ce projet pour obtenir la note maximale de 16/16.
-		</span><br>
-		<span class="text">
-			<u style="font-weight: bold">Nature</u> : Projet d'étude (SAE).
-		</span><br>
-		<span class="text">
-			<u style="font-weight: bold">Date</u> : Octobre 2023 - Février 2024.
-		</span><br>
-		<span class="text">
-			<u style="font-weight: bold">Activité</u> : Dévellopeur principale et chargé de mission du BDE Info Contact.
-	 	</span><br>
-		<span class="text">
-			<u style="font-weight: bold">Repo</u> : Public - <a href="https://github.com/vadim-soude/SAE-S5-Front">Front</a> | <a href="https://github.com/vadim-soude/SAE-S5-Back">Back</a>.
-	 	</span><br>
-		<span class="text">
-			<u style="font-weight: bold">Compétences </u> :
+			<span style="font-weight: bold">Activité</span> : Dévellopeur principale et chargé de mission du BDE Info Contact.
 	 	</span>
-		<div class="list">
-			<ul>
-				<li>- <span style="font-weight: bold" >Programmation orientée objet :</span> C#, TypeScript</li>
-				<li>- <span style="font-weight: bold" >Frameworks utilisé :</span> Angular, AspNetCore.MVC, dotnet.EntityFramework</li>
-				<li>- <span style="font-weight: bold" >Gestion de dépendances :</span> NPM, nuggets</li>
-				<li>- <span style="font-weight: bold" >Version Control :</span> Github</li>
-				<li>- <span style="font-weight: bold" >Déploiement de service :</span> Angular / Nginx, DotNet</li>
-				<li>- <span style="font-weight: bold" >Base de données :</span> Mysql, ORM en C#</li>
-				<li>- <span style="font-weight: bold" >Mise en production :</span> Docker, DockerFile custom et docker compose</li>
-				<li>- <span style="font-weight: bold" >Système :</span> UNIX</li>
-			</ul>
+		<span class="text">
+			<span style="font-weight: bold">Date</span> : Octobre 2023 - Février 2024.
+		</span>
+		<div class="dropdown">
+			<input class="dropdown-input" id="toggleDropdown7" type="checkbox">
+			<label class="dropdown-label" for="toggleDropdown7"><span class="text"><span
+				style="font-weight: bold">Informations</span> : Projet de réalisation d'un site d'information et de vente du BDE de mon IUT.</span></label>
+			<div class="dropdown-content">
+				<span class="text">
+					Ce projet a été réalisé en équipe dans le cadre de la SAE du second semestre de ma troisième année de BUT Informatique. <br>
+					Ce projet m'a permis d'apprendre plusieurs nouveaux frameworks et environnement de travail. <br>
+					Je me suis investi au maximum dans ce projet pour obtenir la note maximale de 16/16.
+				</span>
+			</div>
 		</div>
+		<div class="dropdown">
+			<input class="dropdown-input" id="toggleDropdown8" type="checkbox">
+			<label class="dropdown-label" for="toggleDropdown8"><span class="text"><span
+				style="font-weight: bold">Compétences acquises ou appliquées</span> :</span></label>
+			<div class="dropdown-content">
+				- <span style="font-weight: bold">Programmation orientée objet :</span> C#, TypeScript<br>
+				- <span style="font-weight: bold">Frameworks utilisé :</span> Angular, AspNetCore.MVC, dotnet.EntityFramework<br>
+				- <span style="font-weight: bold">Gestion de dépendances :</span> NPM, nuggets<br>
+				- <span style="font-weight: bold">Version Control :</span> Github<br>
+				- <span style="font-weight: bold">Déploiement de service :</span> Angular / Nginx, DotNet<br>
+				- <span style="font-weight: bold">Base de données :</span> Mysql, ORM en C#<br>
+				- <span style="font-weight: bold">Mise en production :</span> Docker, DockerFile custom et docker compose<br>
+				- <span style="font-weight: bold">Système :</span> UNIX<br>
+			</div>
+		</div>
+		<span class="text">
+			<span style="font-weight: bold">Nature</span> : Projet d'étude (SAE).
+		</span>
+		<span class="text">
+			<span style="font-weight: bold">Repo</span> : Public - <a href="https://github.com/vadim-soude/SAE-S5-Front">Front</a> | <a
+			href="https://github.com/vadim-soude/SAE-S5-Back">Back</a>.
+	 	</span>
+	</div>
 	</div>
 </section>
 
 <style>
-	.info-box{
-			display: flex;
-			flex-direction: column;
-      padding-left: 5vh;
-			margin-bottom: 5vh;
-	}
-	ul{
-      padding-left: 2vh;
-	}
-	.text{
-			font-size: 20px;
-	}
-	li{
-			font-size: 15px;
-      white-space: nowrap;
-	}
+    .info-box {
+        display: flex;
+        flex-direction: column;
+        padding-left: 5vh;
+        margin-bottom: 5vh;
+    }
+
+    ul {
+        padding-left: 2vh;
+    }
+
+    .text {
+        font-size: 20px;
+				padding-top: 15px;
+    }
+
+    li {
+        font-size: 15px;
+        white-space: nowrap;
+    }
+
+    .dropdown {
+        position: relative;
+        padding-top: 15px;
+    }
+
+    .dropdown-input {
+        display: none;
+    }
+
+    .dropdown-label {
+        cursor: pointer;
+        position: relative;
+    }
+
+    .dropdown-label::after {
+        content: '\25BC';
+        margin-left: 5px;
+    }
+
+    .dropdown-content {
+        display: none;
+        position: absolute;
+        box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+        min-width: 160px;
+        padding: 10px;
+        z-index: 1;
+    }
+
+    .dropdown-input:checked + .dropdown-label::after {
+        content: '\25B2'; /* flèche vers le haut Unicode */
+    }
+
+    .dropdown-input:checked + .dropdown-label + .dropdown-content {
+        display: block;
+    }
 </style>
